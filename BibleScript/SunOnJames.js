2676e8b60c8a612b2f3f49903480f16e998a4f6e
The SunShining <51297196+TheSunShiningDev@users.noreply.github.com>
2019-10-26 22:47:46 -0600

    minor edits

--- BibleScript/SunOnJames.js
+++ BibleScript/SunOnJames.js
@@ -1,1 +1,23 @@
 //SunShine On James JS File
+window.onload  = function() {
+    var C1V1W1 = document.getElementById("C1V1W1").addEventListener("click", function hideC1V1W1(){document.getElementById("C1V1W1").innerHTML = "__"});
+    var C1V1W2 = document.getElementById("C1V1W2").addEventListener("click", function hideC1V1W2(){document.getElementById("C1V1W2").innerHTML = "_"});
+    var C1V1W3 = document.getElementById("C1V1W3").addEventListener("click", function hideC1V1W3(){document.getElementById("C1V1W3").innerHTML = "__"});
+    var C1V1W4 = document.getElementById("C1V1W4").addEventListener("click", function hideC1V1W4(){document.getElementById("C1V1W4").innerHTML = "_ _"});
+    var C1V1W5 = document.getElementById("C1V1W5").addEventListener("click", function hideC1V1W5(){document.getElementById("C1V1W5").innerHTML = "__"});
+    var C1V1W6 = document.getElementById("C1V1W6").addEventListener("click", function hideC1V1W6(){document.getElementById("C1V1W6").innerHTML = "__"});
+    var C1V1W7 = document.getElementById("C1V1W7").addEventListener("click", function hideC1V1W7(){document.getElementById("C1V1W7").innerHTML = "__"});
+    var C1V1W8 = document.getElementById("C1V1W8").addEventListener("click", function hideC1V1W8(){document.getElementById("C1V1W8").innerHTML = "__"});
+    var C1V1W9 = document.getElementById("C1V1W9").addEventListener("click", function hideC1V1W9(){document.getElementById("C1V1W9").innerHTML = "__"});
+    var C1V1W10 = document.getElementById("C1V1W10").addEventListener("click", function hideC1V1W10(){document.getElementById("C1V1W10").innerHTML = "__"});
+    var C1V1W11 = document.getElementById("C1V1W11").addEventListener("click", function hideC1V1W11(){document.getElementById("C1V1W11").innerHTML = "__"});
+    var C1V1W12 = document.getElementById("C1V1W12").addEventListener("click", function hideC1V1W12(){document.getElementById("C1V1W12").innerHTML = "_"});
+    var C1V1W13 = document.getElementById("C1V1W13").addEventListener("click", function hideC1V1W13(){document.getElementById("C1V1W13").innerHTML = "__"});
+    var C1V1W14 = document.getElementById("C1V1W14").addEventListener("click", function hideC1V1W14(){document.getElementById("C1V1W14").innerHTML = "_ _"});
+    var C1V1W15 = document.getElementById("C1V1W15").addEventListener("click", function hideC1V1W15(){document.getElementById("C1V1W15").innerHTML = "__"});
+    var C1V1W16 = document.getElementById("C1V1W16").addEventListener("click", function hideC1V1W16(){document.getElementById("C1V1W16").innerHTML = "__"});
+    var C1V1W17 = document.getElementById("C1V1W17").addEventListener("click", function hideC1V1W17(){document.getElementById("C1V1W17").innerHTML = "__"});
+    var C1V1W18 = document.getElementById("C1V1W18").addEventListener("click", function hideC1V1W18(){document.getElementById("C1V1W18").innerHTML = "__"});
+    var C1V1W19 = document.getElementById("C1V1W19").addEventListener("click", function hideC1V1W19(){document.getElementById("C1V1W19").innerHTML = "__"});
+    var C1V1W20 = document.getElementById("C1V1W20").addEventListener("click", function hideC1V1W20(){document.getElementById("C1V1W20").innerHTML = "__"});
+};
