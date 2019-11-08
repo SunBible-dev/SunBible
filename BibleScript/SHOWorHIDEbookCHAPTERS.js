2ead3696559fdc474c1017ffdeea5e59d1fb61a2
The SunShining <51297196+TheSunShiningDev@users.noreply.github.com>
2019-11-08 09:11:06 -0700

    cleaning things up a bit.

--- BibleScript/SHOWorHIDEbookCHAPTERS.js
+++ BibleScript/SHOWorHIDEbookCHAPTERS.js
@@ -8,5 +8,13 @@ window.onload  = function() {
     var Hide3 = document.getElementById("Hide3").addEventListener("click", function Hide3(){document.getElementById("LeviticusChapterSelection").style.display = "none";});
     var Show4 = document.getElementById("ShowNumbersChapterSelection").addEventListener("click", function ShowNumbersChapterSelection() {document.getElementById("NumbersChapterSelection").style.display = "block";});
     var Hide4 = document.getElementById("Hide4").addEventListener("click", function Hide4(){document.getElementById("NumbersChapterSelection").style.display = "none";});
+    var Show1 = document.getElementById("ShowGenesisChapterSelection").addEventListener("click", function ShowGenesisChapterSelection() {document.getElementById("GenesisChapterSelection").style.display = "block";});
+    var Hide1 = document.getElementById("Hide1").addEventListener("click", function Hide1(){document.getElementById("GenesisChapterSelection").style.display = "none";});
+    var Show2 = document.getElementById("ShowExodusChapterSelection").addEventListener("click", function ShowExodusChapterSelection() {document.getElementById("ExodusChapterSelection").style.display = "block";});
+    var Hide2 = document.getElementById("Hide2").addEventListener("click", function Hide2(){document.getElementById("ExodusChapterSelection").style.display = "none";});
+    var Show3 = document.getElementById("ShowLeviticusChapterSelection").addEventListener("click", function ShowLeviticusChapterSelection() {document.getElementById("LeviticusChapterSelection").style.display = "block";});
+    var Hide3 = document.getElementById("Hide3").addEventListener("click", function Hide3(){document.getElementById("LeviticusChapterSelection").style.display = "none";});
+    var Show4 = document.getElementById("ShowNumbersChapterSelection").addEventListener("click", function ShowNumbersChapterSelection() {document.getElementById("NumbersChapterSelection").style.display = "block";});
+    var Hide4 = document.getElementById("Hide4").addEventListener("click", function Hide4(){document.getElementById("NumbersChapterSelection").style.display = "none";});
    
 };
