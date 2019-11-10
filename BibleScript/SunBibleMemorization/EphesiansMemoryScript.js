1ddd4db207c2c8f7b9623e0c2e5df3a698c4b85b
The SunShining <51297196+TheSunShiningDev@users.noreply.github.com>
2019-11-09 18:03:22 -0700

    fixed memory script for e.

--- BibleScript/SunBibleMemorization/EphesiansMemoryScript.js
+++ BibleScript/SunBibleMemorization/EphesiansMemoryScript.js
@@ -1,9 +1,38 @@
 // Java Script for SUNBIBLE Memorization Guide
 $(document).ready(function(){
-    var words = $( ".M" ).text().split( /\s+/ );
+    var words = $( "#Ephesians1" ).text().split( /\s+/ );
     var text = words.join( "</span> <span>" );
-    $( ".M" ).html( "<span>" + text + "</span>" );
     $( "span" ).on( "click", function() {
-    $( this ).text("___");
+    $( this ).text("___"); 
+    });
+    var words = $( "#Ephesians2" ).text().split( /\s+/ );
+    var text = words.join( "</span> <span>" );
+    $( "#Ephesians2" ).html( "<span>" + text + "</span>" );
+    $( "span" ).on( "click", function() {
+    $( this ).text("___"); 
+    });
+    var words = $( "#Ephesians3" ).text().split( /\s+/ );
+    var text = words.join( "</span> <span>" );
+    $( "#Ephesians3" ).html( "<span>" + text + "</span>" );
+    $( "span" ).on( "click", function() {
+    $( this ).text("___"); 
+    });
+    var words = $( "#Ephesians4" ).text().split( /\s+/ );
+    var text = words.join( "</span> <span>" );
+    $( "#Ephesians4" ).html( "<span>" + text + "</span>" );
+    $( "span" ).on( "click", function() {
+    $( this ).text("___"); 
+    });
+    var words = $( "#Ephesians5" ).text().split( /\s+/ );
+    var text = words.join( "</span> <span>" );
+    $( "#Ephesians5" ).html( "<span>" + text + "</span>" );
+    $( "span" ).on( "click", function() {
+    $( this ).text("___"); 
+    });
+    var words = $( "#Ephesians6" ).text().split( /\s+/ );
+    var text = words.join( "</span> <span>" );
+    $( "#Ephesians6" ).html( "<span>" + text + "</span>" );
+    $( "span" ).on( "click", function() {
+    $( this ).text("___"); 
     });
 });
