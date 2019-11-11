f1867ffab1421e92627facab4a9d6b4eac5078f5
The SunShining <51297196+TheSunShiningDev@users.noreply.github.com>
2019-11-10 23:30:03 -0700

    MEMORYSTUFFFORjAMES

---
+++ BibleScript/SunBibleMemorization/SunOnJamesMemoryScript.js
@@ -0,0 +1,33 @@
+// Java Script for SUNBIBLE Memorization Guide
+$(document).ready(function(){
+$(".M").click(function(){$( this ).css("background-color", "black");});
+$("#Level1").click(function(){$(".R1").css("background-color", "black");});
+$("#Level2").click(function(){$(".R2").css("background-color", "black");});
+$("#Level3").click(function(){$(".R3").css("background-color", "black");});
+$("#Level4").click(function(){$(".R4").css("background-color", "black");});
+$("#Level5").click(function(){$(".R5").css("background-color", "black");});
+$(".MM").click(function(){$( this ).css("background-color", "black");});
+$("#LLevel1").click(function(){$(".RR1").css("background-color", "black");});
+$("#LLevel2").click(function(){$(".RR2").css("background-color", "black");});
+$("#LLevel3").click(function(){$(".RR3").css("background-color", "black");});
+$("#LLevel4").click(function(){$(".RR4").css("background-color", "black");});
+$("#LLevel5").click(function(){$(".RR5").css("background-color", "black");});
+$(".MMM").click(function(){$( this ).css("background-color", "black");});
+$("#LLLevel1").click(function(){$(".RRR1").css("background-color", "black");});
+$("#LLLevel2").click(function(){$(".RRR2").css("background-color", "black");});
+$("#LLLevel3").click(function(){$(".RRR3").css("background-color", "black");});
+$("#LLLevel4").click(function(){$(".RRR4").css("background-color", "black");});
+$("#LLLevel5").click(function(){$(".RRR5").css("background-color", "black");});
+$(".MMMM").click(function(){$( this ).css("background-color", "black");});
+$("#LLLLevel1").click(function(){$(".RRRR1").css("background-color", "black");});
+$("#LLLLevel2").click(function(){$(".RRRR2").css("background-color", "black");});
+$("#LLLLevel3").click(function(){$(".RRRR3").css("background-color", "black");});
+$("#LLLLevel4").click(function(){$(".RRRR4").css("background-color", "black");});
+$("#LLLLevel5").click(function(){$(".RRRR5").css("background-color", "black");});
+$(".MMMMM").click(function(){$( this ).css("background-color", "black");});
+$("#LLLLLevel1").click(function(){$(".RRRRR1").css("background-color", "black");});
+$("#LLLLLevel2").click(function(){$(".RRRRR2").css("background-color", "black");});
+$("#LLLLLevel3").click(function(){$(".RRRRR3").css("background-color", "black");});
+$("#LLLLLevel4").click(function(){$(".RRRRR4").css("background-color", "black");});
+$("#LLLLLevel5").click(function(){$(".RRRRR5").css("background-color", "black");});
+});
