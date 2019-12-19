c4126dd5fe6b0b15fb88c0f11b85366d711adbe4
The SunShining <51297196+TheSunShiningDev@users.noreply.github.com>
2019-12-19 08:16:48 -0700

    work

---
+++ BibleScript/NEWtestamentBOOKchapters.js
@@ -0,0 +1,4 @@
+// Java Script for SunBible Home Page Showing/Hiding of new Book Chapters
+$(document).ready(function(){
+    $(".ShowChapterSelection").click(function(){$(".ChapterSelection").toggle();});
+});
--- BibleScript/SunBibleMemorization/PhilippiansMemoryScript.js
+++ BibleScript/SunBibleMemorization/PhilippiansMemoryScript.js
@@ -3,25 +3,17 @@ $(document).ready(function(){
     var words = $( "#Philippians1" ).text().split( /\s+/ );
     var text = words.join( "</span> <span>" );
     $( "#Philippians1" ).html( "<span>" + text + "</span>" );
-    $( "span" ).on( "click", function() {
-    $( this ).text("___");
-    });
+    $("span").click(function () { $(this).css("background-color", "black"); });
     var words = $( "#Philippians2" ).text().split( /\s+/ );
     var text = words.join( "</span> <span>" );
     $( "#Philippians2" ).html( "<span>" + text + "</span>" );
-    $( "span" ).on( "click", function() {
-    $( this ).text("___");
-    });
+    $("span").click(function () { $(this).css("background-color", "black"); });
     var words = $( "#Philippians3" ).text().split( /\s+/ );
     var text = words.join( "</span> <span>" );
     $( "#Philippians3" ).html( "<span>" + text + "</span>" );
-    $( "span" ).on( "click", function() {
-    $( this ).text("___");
-    });
+    $("span").click(function () { $(this).css("background-color", "black"); });
     var words = $( "#Philippians4" ).text().split( /\s+/ );
     var text = words.join( "</span> <span>" );
     $( "#Philippians4" ).html( "<span>" + text + "</span>" );
-    $( "span" ).on( "click", function() {
-    $( this ).text("___");
-    });
+    $("span").click(function () { $(this).css("background-color", "black"); });
 });
