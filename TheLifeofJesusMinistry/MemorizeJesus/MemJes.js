206307fbce344b1767bb9eb0be3b7fa0463cc828
The SunShining <51297196+TheSunShiningDev@users.noreply.github.com>
2019-12-18 18:23:18 -0700

    JamesVersePics

--- TheLifeofJesusMinistry/MemorizeJesus/MemJes.js
+++ TheLifeofJesusMinistry/MemorizeJesus/MemJes.js
@@ -1,4 +1,5 @@
 // Java Script for SUNBIBLE Memorization Guide Jesus Words
 $(document).ready(function () {
-    $(".red").click(function () { $(this).css("background-color", "red"); });
+    $(".red").on("click", function Memorization() { $(this).css("background-color", "red"); });
+    $("#Undo").click(function () { $(".red").css("background-color", "transparent"); });
 });
