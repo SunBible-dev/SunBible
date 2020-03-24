f919cf655f7238f768fbb211c2d7e2d320467fe6
The SunShining <51297196+TheSunShiningDev@users.noreply.github.com>
2020-03-24 15:43:34 -0600

    more home stuff
    making it look better

--- BibleScript/MoreHomeScript.js
+++ BibleScript/MoreHomeScript.js
@@ -1,1 +1,5 @@
 // Other Script For Home page...
+$(document).ready(function () {
+    $("#ShowOldTestament").click(function () { $("#OldLinkCollection").toggle(); });
+    $("#ShowNewTestament").click(function () { $("#NewLinkCollection").toggle(); });
+});
