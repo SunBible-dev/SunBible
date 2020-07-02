793a1ce85c93f5d61f596cf96066e7d8071c5741
nonresistant <thesunshiningmin@gmail.com>
2020-07-01 18:39:33 -0600

    Torah - Style stuff - and Exodus notes about...

---
+++ BibleScript/Nav3Bar.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+$(".NavBarContainer").click(function(){
+    $(".R2, .RR2, .RRR2, .RRRR2, .RRRRR2").css("background-color", "white");
+    $(".R1, .RR1, .RRR1, .RRRR1, .RRRRR1").css("background-color", "black");
+});
+
+});
