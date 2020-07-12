831702ef353fff01adf80e8e83d16af91acf0881
nonresistant <thesunshiningmin@gmail.com>
2020-07-12 10:22:14 -0600

    index and sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,21 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  'index.html',
+  'BibleStyle/StudyBibleInStyle.css',
+  'BibleScript/SunBibleScript-main.js'
+];
+
 self.addEventListener('install', function(event) {
-    // Perform install steps
-    var CACHE_NAME = 'my-site-cache-v1';
-    var urlsToCache = [
-      '/',
-      'index.html',
-      'BibleStyle/StudyBibleInStyle.css'
-    ];
-    
-    self.addEventListener('install', function(event) {
-      // Perform install steps
-      event.waitUntil(
-        caches.open(CACHE_NAME)
-          .then(function(cache) {
-            console.log('Opened cache');
-            return cache.addAll(urlsToCache);
-          })
-      );
-    });
-    // Perform install steps
-  });
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
 
 
 
