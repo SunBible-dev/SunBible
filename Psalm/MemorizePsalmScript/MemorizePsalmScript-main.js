844b605b0018e490d07a2e0739c03bab8ac16fb9
nonresistant <thesunshiningmin@gmail.com>
2020-07-30 20:24:33 -0600

    Psalm 119:1-8 !!!!!!

--- Psalm/MemorizePsalmScript/MemorizePsalmScript-main.js
+++ Psalm/MemorizePsalmScript/MemorizePsalmScript-main.js
@@ -1,32 +1,32 @@
 $(document).ready(function(){
 
     $(".M").toggle(
-        function(){$( this).css({"background-color": "RGB(92, 254, 59)"});},
+        function(){$( this).css({"background-color": "green"});},
         function(){$( this).css({"background-color": "black"});},
         function(){$( this).css({"background-color": "transparent"});}
       );
 
     $("#LevelStart").click(function(){
-        $(".L1").css("background-color", "blue");
+        $(".L1").css("background-color", "green");
     });
 
     $("#Level1").click(function(){
-        $(".L2").css("background-color", "blue"); 
+        $(".L2").css("background-color", "green"); 
         $(".L1").css("background-color", "black");
     });
 
     $("#Level2").click(function(){
-        $(".L3").css("background-color", "blue");
+        $(".L3").css("background-color", "green");
         $(".L2").css("background-color", "black");
     });
 
     $("#Level3").click(function(){
-        $(".L4").css("background-color", "blue");
+        $(".L4").css("background-color", "green");
         $(".L3").css("background-color", "black");
     });
 
     $("#Level4").click(function(){
-        $(".L5").css("background-color", "blue");
+        $(".L5").css("background-color", "green");
         $(".L4").css("background-color", "black");
     });
 
