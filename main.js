c2804d39a6e01292d61b7b7ad89498487950593a
Nathan <thesunshiningmin@gmail.com>
2020-09-10 21:25:21 -0600

    Psalm 45-47 mem

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
+//I have no idea what most of this does!!! - NCR
+
+
+
+
 // Retrieve information about screen size, displays, cursor position, etc.
 //
 // For more info, see:
 // https://electronjs.org/docs/api/screen
 
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu, session } = require('electron')
 
-app.on('ready', () => {
+const path = require('path')
+
+app.on('ready', async () => {
   // We cannot require the screen module until the
   // app is ready
   const { screen } = require('electron')
@@ -24,4 +31,56 @@ app.on('ready', () => {
   })
 
   mainWindow.loadFile('docs/index.html')
+
+
+  // SunBuble Chrome Ex
+  //await session.defaultSession.loadExtension(path.join(__dirname, 'docs'))
+
+
+
 })
+
+
+
+
+
+
+
+
+
+
+// SunBible menu
+
+
+const template = [
+
+  {
+    label: 'SunBible',
+    submenu: [
+    // {
+    //    label: 'SunBible App Home',
+    //    click: async () => {
+    //      const { shell } = require('electron')
+    //      await shell.BrowserWindow.loadFile('docs/index.html')
+    //    }
+    //  },
+    { role: 'copy' },
+      { type: 'separator' },
+      {
+        label: 'View SunBible Online',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://the-sunshining.github.io/SunBible/')
+        }
+      }
+    ]
+  },
+
+  
+
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
+
+
