e5c0d0ac79d2daadb00ba1fdc2e0d42aa3047807
TheSunShining <thesunshiningmin@gmail.com>
2020-09-17 21:20:05 -0600

    mem psalm 70-80

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
 //I have no idea what most of this does!!! - NCR
 
-// Retrieve information about screen size, displays, cursor position, etc.
-//
-// For more info, see:
-// https://electronjs.org/docs/api/screen
-
 const { app, BrowserWindow, Menu, session } = require('electron')
 
 
+
+
+// Main SunBible App Activity: 
 app.on('ready', async () => {
   // We cannot require the screen module until the
   // app is ready
   const { screen } = require('electron')
 
-  // Create a window that fills the sceen's available
-  // work area.
+  // Create a window that fills the sceen's available  work area.
   const primaryDisplay = screen.getPrimaryDisplay()
   const { width, height } = primaryDisplay.workAreaSize
 
