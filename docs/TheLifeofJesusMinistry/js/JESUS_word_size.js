91a67c6b52ab29bff00e8682c086615a3d478ea0
TheSunShining <thesunshiningmin@gmail.com>
2020-10-20 17:50:57 -0700

    jesus words only and some changes

---
+++ docs/TheLifeofJesusMinistry/js/JESUS_word_size.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    
+
+    $("#20px").click(function () { $(".red").css("font-size", "20px"); });
+    $("#25px").click(function () { $(".red").css("font-size", "25px"); });
+    $("#30px").click(function () { $(".red").css("font-size", "30px"); });
+    $("#35px").click(function () { $(".red").css("font-size", "35px"); });
+    $("#40px").click(function () { $(".red").css("font-size", "40px"); });
+    $("#45px").click(function () { $(".red").css("font-size", "45px"); });
+    $("#50px").click(function () { $(".red").css("font-size", "50px"); });
+    $("#60px").click(function () { $(".red").css("font-size", "60px"); });
+    $("#70px").click(function () { $(".red").css("font-size", "70px"); });
+    $("#80px").click(function () { $(".red").css("font-size", "80px"); });
+    $("#90px").click(function () { $(".red").css("font-size", "90px"); });
+
+    $("#100px").click(function () { $(".red").css("font-size", "100px"); });
+    $("#200px").click(function () { $(".red").css("font-size", "200px"); });
+
+});
