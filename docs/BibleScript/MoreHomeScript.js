424833ac2bd888103b313cb8e5a6e1d2329fc8b4
TheSunShining <thesunshiningmin@gmail.com>
2020-11-19 11:05:39 -0700

    some style changes and script delete!!!

--- docs/BibleScript/MoreHomeScript.js
+++
@@ -1,5 +0,0 @@
-// Other Script For Home page...
-$(document).ready(function () {
-    $("#ShowOldTestament").click(function () { $("#OldLinkCollection").toggle(); });
-    $("#ShowNewTestament").click(function () { $("#NewLinkCollection").toggle(); });
-});
