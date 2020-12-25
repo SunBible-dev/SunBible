4faabe57452903a0c99c9b89952decf9007264a2
The SunShining <thesunshiningmin@gmail.com>
2020-12-25 10:41:04 -0600

    mac app menu

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //I have no idea what most of this does!!! - NCR
 
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 
 
 // Main App Activity: 
@@ -27,6 +27,55 @@ app.on('ready', async () => {
 
 })
 
+
+
+//App menu mac os
+const isMac = process.platform === 'darwin'
+
+const template = [
+  // { role: 'appMenu' }
+  ...(isMac ? [{
+    label: app.name,
+    submenu: [
+      {
+        label: 'View SunBible Online',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://the-sunshining.github.io/SunBible/')
+        }
+      },
+      {
+        label: 'Open SunBible Wallpaper online',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://the-sunshining.github.io/SunBible_IMG_Library/SunBible_wallpaper_img/wallpaper.html')
+        }
+      },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideothers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  }] : []),
+ 
+  
+  // { role: 'viewMenu' }
+  ...(isMac ? [{
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  }] : [])
+ 
+  
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
 
 
 
