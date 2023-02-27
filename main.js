c82b49741a502ae839f29f4c1d4ddf1fe41deb8c
The SunShining <thesunshiningmin@gmail.com>
2023-02-26 18:01:16 -0600

    update

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ app.on('ready', async () => {
         }
     })
 
-    mainWindow.loadFile('docs/app-iframe.html')
+    mainWindow.loadFile('BibleBase/app-iframe.html')
     mainWindow.removeMenu()
 
 
@@ -35,42 +35,6 @@ const template = [
   ...(isMac ? [{
     label: app.name,
     submenu: [
-      {
-        label: 'View SunBible Online',
-        click: async () => {
-          const { shell } = require('electron')
-          await shell.openExternal('https://the-sunshining.github.io/SunBible/')
-        }
-      },
-      {
-        label: 'Open SunBible Wallpaper online',
-        click: async () => {
-          const { shell } = require('electron')
-          await shell.openExternal('https://the-sunshining.github.io/SunBible_IMG_Library/SunBible_wallpaper_img/wallpaper.html')
-        }
-      },
-      {
-        label: 'Join the SunBible discord server',
-        click: async () => {
-          const { shell } = require('electron')
-          await shell.openExternal('https://discord.gg/t8VryNcGgc')
-        }
-      },
-      {
-        label: 'Read SunBible updates and more on medium',
-        click: async () => {
-          const { shell } = require('electron')
-          await shell.openExternal('https://medium.com/sunbible')
-        }
-      },
-      {
-        label: 'Check for updates',
-        click: async () => {
-          const { shell } = require('electron')
-          await shell.openExternal('https://github.com/the-sunshining/SunBible/releases')
-        }
-      },
-      { type: 'separator' },
       { role: 'hide' },
       { role: 'hideothers' },
       { role: 'unhide' },
