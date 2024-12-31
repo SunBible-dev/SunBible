ad4f0769155f27ea2fdc5bc6d5c4d9479a1f3abe
The SunShining <thesunshiningmin@gmail.com>
2024-12-31 12:23:02 -0700

    big delete, going in new direction for app iframe!!!

--- BibleBase/appTheme/js/android-menu.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.menu').forEach(function(element) {
-    element.addEventListener('click', function() {
- 
-    document.querySelector('header').style.display = 'block';
-    document.getElementById('divFORiframe').style.display = 'block';
-    document.getElementById('READNOW').style.display = 'none';
-});
-});
